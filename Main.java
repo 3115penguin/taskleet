1374dbe94b478d8c9457c66e7af4f833f847488a
penguinos <anna-alekseevap@yandex.ru>
2025-04-29 09:36:09 +0300

    ;c9d97d9[f79[u

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hiii14188352ii");
+        System.out.println(
+                "p9hp[8h08[08g[8g88352ii");
     }
 }
